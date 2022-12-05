f9f96b61de6d6e25f5ba2c7a4fcbb8648d066d77
Marcelo Penholato <mpenholato@hotmail.com>
2022-12-04 23:52:34 -0300

    Update

--- ContaBanco/src/com/banco/ContaTerminal.java
+++ ContaBanco/src/com/banco/ContaTerminal.java
@@ -1,5 +1,6 @@
 package com.banco;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -7,9 +8,9 @@ public class ContaTerminal {
 	public static void main(String[] args) {
         //TODO:Conhecer e importar a classe Scanner
         Scanner sc = new Scanner(System.in);
-        
-        String nomeCliente = " ";
+        sc.useLocale(Locale.ENGLISH);
 
+            
         //Exibir as mensagens para o novo usuário
         System.out.println("Por favor, digite o n�mero da Ag�ncia !");
         String agencia = sc.nextLine();
@@ -19,8 +20,7 @@ public class ContaTerminal {
         
         System.out.println("Por favor, digite o nome do cliente !");
         sc.nextLine();
-        nomeCliente+= sc.nextLine();
-
+        String nomeCliente = sc.nextLine();
         
         System.out.println("Por favor, digite o saldo da conta !");
         double saldo = sc.nextDouble();
@@ -29,7 +29,7 @@ public class ContaTerminal {
 
         //Imprimir na tela as informações do cliente.
         System.out.println("Ol� "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia  é" 
-        					+ agencia+", conta "+numero+" e seu saldo R$:"+ saldo + " ja esta disponível para saque");
+        			+ agencia+", conta "+numero+" e seu saldo R$:"+ saldo + " ja esta disponível para saque");
 
 
 	}
